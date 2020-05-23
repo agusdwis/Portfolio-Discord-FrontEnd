eda8a4864788f99f7b23fe085a730cefbcd298ad
agusdwis <agusdwi@alterra.id>
2020-05-23 12:56:28 +0700

    fix profile section

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -7,14 +7,13 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Grid from "@material-ui/core/Grid";
-import IconButton from "@material-ui/core/IconButton";
 
 const useStyles = makeStyles({
     root: {
         maxWidth: 400,
         flexGrow: 1,
         borderRadius: 10,
-        border: 'none'
+        border: 'none',
     },
     media: {
         height: 160,
@@ -47,11 +46,11 @@ const useStyles = makeStyles({
         height: '10vmin'
     },
     icon1:{
-        fontSize: '5px',
+        fontSize: '15px',
         color: '#4FB581',
     },
     icon2:{
-        fontSize: '5px',
+        fontSize: '15px',
         color: '#B9BBBE'
     },
     descDetail:{
@@ -84,21 +83,17 @@ export default function MediaCard() {
                         <Grid container spacing={1}>
                             <Grid item xs={6}>
                                 <Grid className={classes.descDetail} container spacing={1}>
-                                    <IconButton className={classes.icon1} aria-label="online">
-                                        <FiberManualRecordIcon fontSize="small" />
-                                    </IconButton>
+                                    <FiberManualRecordIcon className={classes.icon1} fontSize="small" />
                                     <Typography className={classes.fontColor}>
-                                        157.234 Online
+                                        &nbsp;157.234 Online
                                     </Typography>
                                 </Grid>
                             </Grid>
                             <Grid item xs={6}>
                                 <Grid className={classes.descDetail} container spacing={1}>
-                                    <IconButton className={classes.icon2} aria-label="member">
-                                        <FiberManualRecordIcon fontSize="small" />
-                                    </IconButton>
+                                    <FiberManualRecordIcon className={classes.icon2} fontSize="small" />
                                     <Typography className={classes.fontColor}>
-                                        344.345 Members
+                                        &nbsp;344.345 Members
                                     </Typography>
                                 </Grid>
                             </Grid>
