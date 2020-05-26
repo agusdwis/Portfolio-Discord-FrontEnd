ec20b6a178759d52df3f1233a9159f32c1bf32fa
agusdwis <agusdwi@alterra.id>
2020-05-26 13:10:11 +0700

    app bar chat

--- src/pages/GroupPage.jsx
+++ src/pages/GroupPage.jsx
@@ -6,6 +6,10 @@ import Grid from "@material-ui/core/Grid";
 
 import '../assets/styles/chatpages.css';
 import ChatAppBar from "../components/ChatAppBar";
+import GuildDiscovery from "../components/GuildDiscovery";
+import ChatBar2 from "./ChatPage";
+import Container from "@material-ui/core/Container";
+import {GiftedChat} from "react-web-gifted-chat";
 
 const useStyles = () => ({
     root: {
@@ -13,14 +17,42 @@ const useStyles = () => ({
     },
     content: {
         flexGrow: 1,
+        position: 'static'
     },
     chats: {
         backgroundColor: '#36393F',
-        minHeight: '800px'
+        minHeight: '800px',
     }
 });
 
 class Group extends React.Component {
+    state = {
+        messages: [],
+    };
+
+    componentWillMount() {
+        this.setState({
+            messages: [
+                {
+                    id: 1,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        id: 2,
+                        name: 'React',
+                        avatar: 'https://facebook.github.io/react/img/logo_og.png',
+                    },
+                },
+            ],
+        });
+    }
+
+    onSend(messages = []) {
+        this.setState((previousState) => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }));
+    }
+
     render() {
         const { classes } = this.props;
         return(
@@ -35,15 +67,22 @@ class Group extends React.Component {
 
                             <Grid className={classes.chats} item xs={12} lg={10}>
                                 <ChatAppBar/>
-
-                                <Grid container spacing={0}>
-                                    <Grid item xs={8} lg={9}>
-                                        Chats
+                                <Container style={{paddingRight: 0}}>
+                                    <Grid container spacing={0}>
+                                        <Grid item xs={8} lg={9}>
+                                            <GiftedChat
+                                                messages={this.state.messages}
+                                                onSend={(messages) => this.onSend(messages)}
+                                                user={{
+                                                    id: 1,
+                                                }}
+                                            />
+                                        </Grid>
+                                        <Grid style={{backgroundColor: '#2F3136'}} item xs={4} lg={3}>
+                                            Member Lists
+                                        </Grid>
                                     </Grid>
-                                    <Grid item xs={4} lg={3}>
-                                        Member Lists
-                                    </Grid>
-                                </Grid>
+                                </Container>
                             </Grid>
                         </Grid>
                     </main>
