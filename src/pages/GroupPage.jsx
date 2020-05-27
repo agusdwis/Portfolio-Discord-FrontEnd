3aa66accf1870a4828ea5a440849eba42b5ecb3d
agusdwis <agusdwi@alterra.id>
2020-05-27 18:39:37 +0700

    update dikit

--- src/pages/GroupPage.jsx
+++ src/pages/GroupPage.jsx
@@ -176,13 +176,14 @@ class Group extends React.Component {
                                     <Grid className={classes.chatSection} item xs={12} lg={8}>
                                         <Paper elevation={0} classes={{root:classes.chatPaper}}>
                                             <ChatList/>
-                                            <GiftedChat
-                                                messages={this.state.messages}
-                                                onSend={(messages) => this.onSend(messages)}
-                                                user={{ id: 1, name:'Johny'}}
-                                                isAnimated={true}
-                                                showUserAvatar={true}
-                                            />
+
+                                            {/*<GiftedChat*/}
+                                            {/*    messages={this.state.messages}*/}
+                                            {/*    onSend={(messages) => this.onSend(messages)}*/}
+                                            {/*    user={{ id: 1, name:'Johny'}}*/}
+                                            {/*    isAnimated={true}*/}
+                                            {/*    showUserAvatar={true}*/}
+                                            {/*/>*/}
                                         </Paper>
 
                                         <Grid container className={classes.containerForm}>
